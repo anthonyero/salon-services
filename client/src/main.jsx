aafc10fb2ba7f23c72a24cb7477125603baead49
Anthony Rodriguez-Ortiz <anthonyero@gmail.com>
2024-05-15 22:28:13 -0500

    AERO: Made modifications to `App.jsx`, `main.jsx`, `vite.config.js`, and added a `NotFound.jsx` for testing. The client successfully deployed with these modifications

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
+import NotFound from './pages/NotFound';
 import './index.css'
 
-const router = createBrowserRouter([
-  { 
-    path: '/',
-    element: <App />,
-
-    children: [
-      {
-        index: true,
-        element: <App /> // Change to <Home /> once built
-      }
-    ]
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     errorElement: <NotFound />,
+//     children: [
+//       {
+//         index: true,
+//         element: <NotFound /> // Change to Home when written
+//       },
+//     ],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={rout}
-)
+  // <RouterProvider router={router} />
+  <App />
+);
