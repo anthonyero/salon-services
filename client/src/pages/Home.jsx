53cba41af09c2affacd5e59863a3511ea4b83945
Anthony Rodriguez-Ortiz <anthonyero@gmail.com>
2024-05-16 19:47:17 -0500

    AERO: Defined simple Home, landing page and incorporated into React Router

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+	// About
+	// Services
+	// Gallery
+	// Sign up / sign in
+	// Booking link
+	// Experiences and inspos
+	// Social Media Links
+	// Contact 
+import { Link } from 'react-router-dom';
+// import { useQuery } from '@apollo/client';
+// import { QUERY_ } from '../utils/queries'; // Query of available services 
+
+// import Nav from ''
+
+const Home = () => {
+
+	return (
+		<main>
+			<h2>Landing Page</h2>
+		</main>
+	)
+};
+
+export default Home;
