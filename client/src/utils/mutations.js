0c3eee915c950baa6a0915d31226d48c8a7826e0
Anthony Rodriguez-Ortiz <anthonyero@gmail.com>
2024-05-16 21:26:27 -0500

    AERO: Defined our `ADD_USER` variable, in 'client' `mutations.js`, referencing our GQL mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
-  mutation addUser(firstName: String!, lastName: String!, email: String!, username: String!, password: String!) {
+  mutation addUser($firstName: String!, $lastName: String!, $email: String!, $username: String!, $password: String!) {
     addUser(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password) {
       token
       user {
