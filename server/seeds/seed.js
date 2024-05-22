e98c3f1e2232af17e4f125ee49e9e420cd117422
anthonyero <anthonyero@gmail.com>
2024-05-22 17:16:23 -0500

    AERO: WIP, attempting to push the generated appoints to their respective users appointments array but encountering issues with querying as nothing is being returned

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -20,11 +20,11 @@ db.once('open', async () => {
     // Retrieve 
     const nonArtistUsers = await User.find({ artist: false });
     const artistUser = await User.find({ artist: true })
-    console.log(`nonArtistUsers: ${nonArtistUsers}`);
-    console.log(`artistUser: ${artistUser}`);
+    // console.log(`nonArtistUsers: ${nonArtistUsers}`);
+    // console.log(`artistUser: ${artistUser}`);
 
     const services = await Service.find();
-    console.log(services)
+    // console.log(services)
 
     // Scheduling appointments
     await Appointment.insertMany([
@@ -46,19 +46,37 @@ db.once('open', async () => {
     	{
     		user: nonArtistUsers[2]['_id'],
     		services: [services[0]['_id'], services[2]['_id']],
-    		apptDate: new Date('2024-07-04T09:30:00-05:00'),
+    		apptDate: new Date('2024-06-13T09:30:00-05:00'),
     		completed: false,
     		artist: artistUser[0]['_id']
     	},
     	{
     		user: nonArtistUsers[0]['_id'],
     		services: [services[2]['_id']],
-    		apptDate: new Date('2024-05-05T09:30:00-05:00'),
+    		apptDate: new Date('2024-06-05T09:30:00-05:00'),
     		requests: 'Coming in for a touchup',
     		completed: false,
     		artist: artistUser[0]['_id']
     	}
     	]);
+
+    // Appointments are NOT automatically populated within the parent User records
+    const storedAppointments = await Appointment.find();
+    storedAppointments.map(async (appointment) => {
+    	// Update the user's appointments array
+    	console.log(appointment)
+    	const updatedUser = await User.findByIdAndUpdate(appointment.user,
+    		{ $push: { appointments : appointment._id} },
+    		{ new: true }
+    		);
+    	console.log(updatedUser);
+    	// Update the artist's appointments array
+    	const updatedArtist = await User.findByIdAndUpdate(appointment.artist,
+    		{ $push: { appointments : appointment._id} },
+    		{ new: true }
+    		);
+    	console.log(updatedArtist)
+    })
 
 
     console.log('all done!');
