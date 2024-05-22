863a5c86a39ad51778e58acdfd7b352c0da3789c
anthonyero <anthonyero@gmail.com>
2024-05-21 21:12:18 -0500

    AERO: Defined the LOGIN_USER mutation in mutations.js within the client directory

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,3 +13,17 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser ($email: String!, $password: String!) {
+      token
+      user {
+        _id
+        firstName
+        lastName
+        username
+      }
+    }
+  }
+`;
