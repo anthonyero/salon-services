94e8e857cf008e7da0b07e1a71d49cdc99239dbf
Youthfulmaster <Dennisangelov3@gmail.com>
2024-05-23 07:07:39 -0700

    Fixed merge conflict/added navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="dropdown menu" data-dropdown-menu>
+          <li className="menu-text">Salon Services</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/gallery">Gallery</Link></li>
+          <li><Link to="/Reviews">Reviews</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li><Link to="/signup">Sign Up</Link></li>
+          <li><Link to="/login">Sign In</Link></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ client/src/pages/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <section>
+      <h2>Gallery</h2>
+      <p>Images showcasing our work...</p>
+    </section>
+  );
+};
+
+export default Gallery;
