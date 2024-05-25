3ad6da118017b077bbf1293d1249363935eac3dd
magicary <behnke.ca@gmail.com>
2024-05-25 00:54:33 -0500

    update css on nav to create responsive menu, need to fix title-bar being visible when it should not be toggled.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+
 const Navbar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="sticky-nav">
-      <div className="top-bar-left">
-        <ul className="dropdown menu" data-dropdown-menu>
-          <li className="menu-text">Salon Services</li>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/gallery">Gallery</Link></li>
-          <li><Link to="/Reviews">Reviews</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
+      <div className="title-bar">
+        <div className="title-bar-title">Menu</div>
+        <button className="menu-icon" type="button" onClick={toggleMenu}>
+        &#9776;
+        </button>
       </div>
-      <div className="top-bar-right">
-        <ul className="menu">
-          <li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/login">Sign In</Link></li>
-        </ul>
+      <div className={`top-bar ${menuOpen ? 'active' : ''}`}>
+        <div className="top-bar-left">
+          <ul className={`dropdown-menu ${menuOpen ? 'active' : ''}`}>
+            <li className="menu-text">Salon Services</li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/gallery">Gallery</Link></li>
+            <li><Link to="/Reviews">Reviews</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className={`menu ${menuOpen ? 'active' : ''}`}>
+            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/login">Sign In</Link></li>
+          </ul>
+        </div>
       </div>
     </div>
   );
