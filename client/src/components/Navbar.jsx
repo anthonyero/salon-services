a587b34972f881c8157dfaa68474620ec9fee969
anthonyero <anthonyero@gmail.com>
2024-05-27 20:32:35 -0500

    AERO: Added a link in the Navbar to `Appointments`

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,6 +26,7 @@ const Navbar = () => {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/services">Services</Link></li>
+            <li><Link to="/request-appointment">Appointments</Link></li>
             <li><Link to="/gallery">Gallery</Link></li>
             <li><Link to="/reviews">Reviews</Link></li>
             <li><Link to="/contact">Contact</Link></li>
