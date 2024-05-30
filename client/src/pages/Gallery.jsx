7ce5a8b15f46a72aeb5f5aff4ca03289ea06dd59
Youthfulmaster <Dennisangelov3@gmail.com>
2024-05-30 09:25:53 -0700

    updated photos to slide in and out

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
-import glitterNails from '/img/glitter nails.jpg';
-import cleaningnails from '/img/pink purples nails.jpg';
-import blackNails from '/img/black nails.jpg';
-import nailpatterns from '/img/nailpatterns.jpg';
-import longNailsPink from '/img/long nails pink.jpg';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './Gallery.css';
+const glitterNails = '/img/glitter nails.jpg';
+const cleaningnails = '/img/pink purples nails.jpg';
+const blackNails = '/img/black nails.jpg';
+const nailpatterns = '/img/nailpatterns.jpg';
+const longNailsPink = '/img/long nails pink.jpg';
 const Gallery = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
     <section className="gallery">
       <h2>Gallery</h2>
       <p>Images showcasing our work...</p>
-      <div className="gallery-grid">
-        <img src={glitterNails} alt="Glitter nails" />
-        <img src={cleaningnails} alt="Cleaning Nails" />
-        <img src={blackNails} alt="Black Nails" />
-        <img src={nailpatterns} alt="Nail Patterns" />
-        <img src={longNailsPink} alt="Long Pink Nails" />
-      </div>
+      <Slider {...settings}>
+        <div>
+          <img src={glitterNails} alt="Glitter nails" />
+        </div>
+        <div>
+          <img src={cleaningnails} alt="Cleaning Nails" />
+        </div>
+        <div>
+          <img src={blackNails} alt="Black Nails" />
+        </div>
+        <div>
+          <img src={nailpatterns} alt="Nail Patterns" />
+        </div>
+        <div>
+          <img src={longNailsPink} alt="Long Pink Nails" />
+        </div>
+      </Slider>
     </section>
   );
 };
